aede71e36ae4ba77264c2966ae04ce2475f97c60
uka-bill <aka.sazali@gmail.com>
2026-06-05 23:16:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2685,4 +2685,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     print(f"🌐 Server will run on port: {port}")
     
-    app.run(host='0.0.0.0', port=8080, debug=False) 
+    app.run(host='0.0.0.0', port=port, debug=False) 
